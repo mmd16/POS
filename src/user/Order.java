e12e8cc64d6cff6dd54173240f9d39ee985e0f60
Vict101 <90037534+Vict101@users.noreply.github.com>
2021-10-26 13:40:35 +0800

    editing

--- src/user/Order.java
+++ src/user/Order.java
@@ -22,26 +22,7 @@ public class Order {
 		this.orderDate = orderDate;				
 		this.deliveryDays = deliveryDays;
 		this.user = user;		
-	}		
-
-	public static Order searchOrder(ArrayList<User> customerList, ArrayList<Order> orderList) {        
-		for (User u : customerList) {        	
-			for (Order o : orderList) {               
-				if (o.getUserName().equals(c.getUserName()))                    
-					return o;           
-			}        
-		}        
-		return null;    
-	}
-	
-	    // encounter bug: "%-9s%-14s%-9s%-26s%\n", the last % causes conversion bug.
-    public static void listOrder(ArrayList<Order> orderList) {
-        System.out.printf("%-14s%-14s%-13s%-13s\n", "Customer Name", "Product Name", "Order Date", "Days");
-        for (Order o : orderList) {
-            System.out.printf("%-9s%-14s%-13s%-13s\n", o.getUserName(), o.getProductName(), o.getStrDate(),
-                    String.valueOf(o.getDeliveryDays()));
-        }
-    }	
+	}			
 
 	public int getDeliveryDays() {		
 		return deliveryDays;	
@@ -68,28 +49,22 @@ public class Order {
 		return productName;
 	}
 	
-	//This is a new add function, it will calculate the suitable response message to customer 
-	//by the predicted order's delivery day and the product's produce date
-	//Here I think that this part can connect with product class' inventory status, when the inputted product' inventory number = 0, 
-        //Then, the staff can create an order for the customer, 
-        //Also, after making many sales payments, if the inventory has reduced to 0, then that product can similarly be created as an order by the staff.
-	//Although it maybe weird in reality, however the project needs to have different inputs and outputs 
 	
-	//Also I want to let the staff input produce date freely rather than only setting it from the beginning.
-	public void suggestMsgToSend(User u, Order o, LocalDate produceDate) { // User is for getting their names
-        LocalDate deliveryPeriod = produceDate.plusDays(this.deliveryDays);  // this is the date after the product is produced and deliver to the customer
-        long daysOfEarly = DAYS.between(deliveryPeriod, o.orderDate);  // this calculate how many days it is earlier than the order date
-        long daysOfDelay = DAYS.between(o.orderDate, deliveryPeriod);   // this calculate how many days it is later than the order date     
-        long refund = 50 * daysOfDelay;
-        
-        // check the date with the orderDate
-        if (deliveryPeriod.equals(o.orderDate)) //equals
-        	System.out.printf("Hello %s%s! Your package will arrive exactly in the order date.", u.distinguishSex(), u.getUsername());
-        else if (deliveryPeriod.isBefore(o.orderDate)) //earlier case
-        	System.out.printf("Hello %s%s! Congratulate that your package will arrive %d days earlier than the orignal date."
-        			, u.distinguishSex(), u.getUsername(), daysOfEarly, getStrDate()); 
-        else if (deliveryPeriod.isAfter(o.orderDate)) // late case
-        	System.out.printf("Hello %s%s! Sorry, your package will be delayed for %d days. You will get $%d as compensation."
-        			, u.distinguishSex(), u.getUsername(), daysOfDelay, refund);
+	public void suggestMsgToSend(String userName, LocalDate produceDate) { // User is for getting their names
+		Order o = Order.searchOrder(userName);
+		LocalDate deliveryPeriod = produceDate.plusDays(this.deliveryDays);  // this is the date after the product is produced and deliver to the customer
+		long daysOfEarly = DAYS.between(deliveryPeriod, o.orderDate);  // this calculate how many days it is earlier than the order date
+		long daysOfDelay = DAYS.between(o.orderDate, deliveryPeriod);   // this calculate how many days it is later than the order date     
+		long refund = 50 * daysOfDelay;
+
+		// check the date with the orderDate
+		if (deliveryPeriod.equals(o.orderDate)) //equals
+			System.out.printf("Hello %s%s! Your package will arrive exactly in the order date.", u.distinguishSex(), u.getUsername());
+		else if (deliveryPeriod.isBefore(o.orderDate)) //earlier case
+			System.out.printf("Hello %s%s! Congratulate that your package will arrive %d days earlier than the orignal date."
+					, u.distinguishSex(), u.getUsername(), daysOfEarly, getStrDate()); 
+		else if (deliveryPeriod.isAfter(o.orderDate)) // late case
+			System.out.printf("Hello %s%s! Sorry, your package will be delayed for %d days. You will get $%d as compensation."
+					, u.distinguishSex(), u.getUsername(), daysOfDelay, refund);
 	}
 }
