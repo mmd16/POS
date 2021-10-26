95f0b25e58c3e7ae1c9b00613bac3cde7e9822fb
Vict101 <90037534+Vict101@users.noreply.github.com>
2021-10-26 10:40:33 +0800

    Update UserManagementController.java
    editing

--- src/controller/UserManagementController.java
+++ src/controller/UserManagementController.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 import staff.Employee;
 import user.User;
+import user.Order;
 
 public class UserManagementController implements Controller {
 	private static User user;
+	private static Order order;
 	private Employee employee; // thinking of separate general staff apart from manager
 	private static UserManagementController instance;
 	
@@ -27,7 +29,7 @@ public class UserManagementController implements Controller {
 	}
 
 	@Override
-	public void execute() {
+	public void execute(String[] cmdParts) {
 		int digit = -1;
 		boolean end = false;
 		System.out.println("Welcome to User Management System");
@@ -35,7 +37,7 @@ public class UserManagementController implements Controller {
 //			System.out.println("Waiting the Card Reader to response...");
 			System.out.println("To continue, please proceed your actions");
 			System.out.println("Input (1) for Membership enquires");
-			System.out.println("Input (2) for Orders Enquires");
+			System.out.println("Input (2) for Creating orders"); // added this part for the staff to input orders
 			System.out.println("Input (3) for Points Enquires");
 			System.out.println("Input (4) to leave");
 			digit = MainController.sc.nextInt();
@@ -43,13 +45,21 @@ public class UserManagementController implements Controller {
 			case 1:
 				System.out.printf("Customer owns a %s membership\n", user.getMembership().getName());
 				break;
-			case 2:
-				user.printOrders();
+			case 2:				
+        			if(cmdParts.length != 4)            
+					return null; // exception               
+				int days = Integer.parseInt(cmdParts[3]);            
+				//r = Record.getInstance();           
+				user.createOrder(cmdParts[0], cmdParts[1], user.ConvertStrToDate(cmdParts[3]), days);            
+				System.out.println("Done.");
 				break;
-			case 3:
-				System.out.printf("The current points in the account is %d\n", user.getPoints());
+			case 3: 
+				order.printOrder();
 				break;
 			case 4:
+				System.out.printf("The current points in the account is %d\n", user.getPoints());
+				break;
+			case 5:
 				System.out.println("Leaving User Management System...");
 				end = true;
 				break;
